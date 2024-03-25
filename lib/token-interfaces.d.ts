fdd155678b2d9cd69214c2c38220aec3c1d8efde
Chris Donnelly <chris.donnelly@coxautoinc.com>
2024-03-25 12:00:18 -0500

    Linting token by adding a concrete Token class.
    Easier to make it work that way by limiting what is exposed in the new token.

---
+++ lib/token-interfaces.d.ts
@@ -0,0 +1,28 @@
+import { HasPosition, Position, TextSpan } from "./position";
+
+export type TriviaFlag = boolean | 'structured'
+
+export interface TokenLike extends HasPosition {
+  /**
+   * The name of the group, as passed to compile.
+   */
+  type?: string | undefined;
+  /**
+   * The match contents.
+   */
+  value: string;
+  /**
+   * The complete match.
+   */
+  text: string;
+  /**
+   * Whether this is considered a "trivia" token (e.g., whitespace, comment).
+   */
+  isTrivia: TriviaFlag;
+}
+
+/**
+ * For matching tokens.  Arrays indicate `OR` matches.
+ */
+export type TokenPattern = TokenLike | string | TokenPattern[] | { value: string?}
+
